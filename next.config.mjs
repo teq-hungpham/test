cff79dbe35605734e8056fd3cc0fc74ffb69336e
teq-hungpham <hungpham@teqnological.asia>
2024-11-05 10:25:54 +0700

    base done

--- next.config.mjs
+++ next.config.mjs
@@ -1,48 +1,57 @@
+/** @type {import('next').NextConfig} */
+
 import dotenv from "dotenv";
 import { execSync } from "child_process";
+import { readdirSync } from "fs";
 
 function getBranch() {
 	const t = execSync("git rev-parse --abbrev-ref HEAD");
-	return t.toString().trim();
+	return t.toString().trim().split("/")[0];
 }
 
-/** @type {import('next').NextConfig} */
+function getFeatures() {
+	const features = readdirSync("./src/features");
+	return features;
+}
+
+function getEnv(config, webpack) {
+	const branch = getBranch();
+
+	dotenv.config({
+		path: `.env.${branch}`,
+	});
+
+	config.plugins.push(
+		new webpack.DefinePlugin({
+			"process.env": JSON.stringify(process.env),
+		}),
+	);
+}
+function replaceUnusedFeatures(config, webpack) {
+	const features = getFeatures();
+
+	features.forEach((feature) => {
+		const envKey = `NEXT_PUBLIC_FEATURE_${feature}`;
+
+		if (!process.env[envKey] || process.env[envKey] === "false") {
+			config.plugins.push(
+				new webpack.NormalModuleReplacementPlugin(
+					new RegExp(`@\\/features\\/${feature}`),
+					`@/features/${feature}/index.fallback`,
+				),
+			);
+		}
+	});
+}
+
 const nextConfig = {
 	output: "export",
 	reactStrictMode: true,
 	webpack: (config, { webpack }) => {
-		const branch = getBranch();
+		getEnv(config, webpack);
+		replaceUnusedFeatures(config, webpack);
 
-    console.log('branch', `.env.${branch}`,);
-    
-
-		dotenv.config({
-			path: `.env.${branch}`,
-		});
-
-		config.plugins.push(
-			new webpack.DefinePlugin({
-				"process.env": JSON.stringify(process.env),
-			}),
-		);
-
-		config.plugins.push(
-			new webpack.NormalModuleReplacementPlugin(
-				/@\/features\/f1/,
-				process.env.NEXT_PUBLIC_F1_ENABLED === "true"
-					? "@/features/f1"
-					: "@/features/f1/index.fallback",
-			),
-			new webpack.NormalModuleReplacementPlugin(
-				/@\/features\/f2/,
-				process.env.NEXT_PUBLIC_F2_ENABLED === "true"
-					? "@/features/f2"
-					: "@/features/f2/index.fallback",
-			),
-		);
-
-    console.log("NEXT_PUBLIC_F1_ENABLED", process.env.NEXT_PUBLIC_F1_ENABLED);
-    
+		console.log("NEXT_PUBLIC_F1_ENABLED", process.env.NEXT_PUBLIC_FEATURE_f1);
 
 		return config;
 	},
